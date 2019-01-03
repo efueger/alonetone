e8875c1ca2dc5f2397d74cd740efe0eeec9b6116
Jenya <jzueva@gmail.com>
2019-01-02 20:14:15 -0500

    Add shoulda-matchers gem configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -108,4 +108,10 @@ RSpec.configure do |config|
   config.expose_dsl_globally = false
 end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 FactoryBot.reload
