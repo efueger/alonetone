07fde4b8fe99b84489e24c62e2373a1eb69432f0
Manfred Stienstra <manfred@fngtps.com>
2019-01-17 14:32:14 +0100

    Remove Paperclip code from Greenfield::AttachedAsset.

--- greenfield/app/models/greenfield/attached_asset.rb
+++ greenfield/app/models/greenfield/attached_asset.rb
@@ -4,25 +4,7 @@ module Greenfield
     has_one :user, through: :post
     has_one :alonetone_asset, through: :post, source: :asset
 
-    # TODO: Need to validate attachment embeds...
-
-    attachment_options = {
-      styles: { original: '' }, # just makes sure original runs through the processor
-      processors: [:mp3_paperclip_processor]
-    }
-
-    if Rails.application.remote_storage?
-      attachment_options[:path] = "/greenfield/:id/:basename.:extension"
-      attachment_options[:s3_permissions] = 'authenticated-read'
-    end
-
-    has_attached_file :mp3, attachment_options
-
     serialize :waveform, Array
-
-    validates_attachment_size :mp3, less_than: 60.megabytes
-    validates_attachment_presence :mp3, message: 'must be set. Make sure you chose a file to upload!'
-    validates_attachment_content_type :mp3, content_type: ['audio/mpeg', 'audio/mp3'], message: " was wrong. It doesn't look like you uploaded a valid mp3 file. Could you double check?"
 
     def permalink
       "#{id}-@attachment"
@@ -31,9 +13,5 @@ module Greenfield
     def length
       Asset.formatted_time(self[:length])
     end
-
-    # This stuff is stubbed for compatibility with Mp3PaperclipProcessor
-    attr_accessor :title
-    def generate_permalink!; end
   end
 end
